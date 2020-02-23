374eaeaed6151440bacca9799eca46f7b968c806
Lukas <lpeyer@gmail.com>
2020-02-23 23:41:12 +0100

    chore(lib): extract dist folder assertion

--- src/commands/project-config/download.js
+++ src/commands/project-config/download.js
@@ -1,5 +1,4 @@
 const fs = require('fs-extra')
-const path = require('path')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const {Command, flags} = require('@oclif/command')
@@ -8,6 +7,7 @@ const sharedFlags = require('../../lib/cli/shared_flags')
 const liApi = require('../../lib/api/livingdocs_api')
 const writeConfig = require('../../lib/write_channel_config')
 const errorReporter = require('../../lib/api/error_reporter')
+const assertDistFolder = require('../../lib/cli/assert_dist_folder')
 
 const description = `Download a project configuration`
 const commandFlags = {
@@ -25,16 +25,9 @@ class DownloadCommand extends Command {
 
   async run () {
     const {token, host, dist, format} = this.parse(DownloadCommand).flags
-    const reportError = errorReporter(this.log, host, {verbose: true})
+    assertDistFolder(dist)
 
-    const workingDir = process.cwd()
-    if (dist && !process.env.LI_SKIP_CWD_CHECK === 'true') {
-      const rootFolder = path.resolve(dist)
-      if (!rootFolder.includes(workingDir)) {
-        this.log(chalk.red(`Error: --dist folder must be in current working directory`))
-        return
-      }
-    }
+    const reportError = errorReporter(this.log, host, {verbose: true})
 
     const result = await liApi.download({host, token})
       .catch(reportError)
@@ -51,6 +44,7 @@ class DownloadCommand extends Command {
 
       // Remove obsolete files
       if (oldFiles && oldFiles.length) {
+        const workingDir = process.cwd()
         this.log('\nObsolete Files:')
         for (const fileName of oldFiles) {
           this.log(fileName.replace(workingDir, '...'))
